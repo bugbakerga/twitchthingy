a7eb5dbd3a6cb1a8e3937cf4bec49fddb44db46a
bugbakerga <wbrenden82@gmail.com>
2021-12-29 17:10:00 -0600

    added laser shooter w/anims

---
+++ TwitchProject2/Assets/Scripts/MainLaser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainLaser : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Animator animator;
+
+    void Start()
+    {
+        StartCoroutine(initialWait());
+    }
+
+    public void PlayAudio(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    IEnumerator initialWait()
+    {
+        yield return new WaitForSeconds(3);
+        animator.SetTrigger("shoot");
+    }
+}
