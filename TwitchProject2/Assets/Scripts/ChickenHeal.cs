b4dff501bfc3d63f4df296499d2eb58e144836c8
bugbakerga <wbrenden82@gmail.com>
2022-07-06 21:35:57 -0500

    nothing

---
+++ TwitchProject2/Assets/Scripts/ChickenHeal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenHeal : MonoBehaviour
+{
+    public float heal;
+
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Chiken")
+        {
+            plyr.gameObject.GetComponent<ChickenHealth>().Heal(heal);
+        }
+    }
+}
