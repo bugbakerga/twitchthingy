ae9592f1ff435846fabe4fbc40d5b456e3e7d349
bugbakerga <wbrenden82@gmail.com>
2022-07-19 19:25:30 -0500

    added lightning soundfx

---
+++ TwitchProject2/Assets/Scripts/Devilkillbox.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Devilkillbox : MonoBehaviour
+{
+    void OnTriggerEnter(Collider plyr)
+    {
+        if (plyr.gameObject.tag == "Chiken")
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            plyr.gameObject.GetComponent<ChickenHealth>().TakeDamage(1f);
+        }
+    }
+}
