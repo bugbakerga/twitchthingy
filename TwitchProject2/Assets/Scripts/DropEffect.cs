0de35ad676bbb34c7a2982d88af6bb92cbd25c3f
bugbakerga <wbrenden82@gmail.com>
2022-12-29 13:18:28 -0600

    added sky map

---
+++ TwitchProject2/Assets/Scripts/DropEffect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropEffect : MonoBehaviour
+{
+    public GameObject dropEffect;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    bool hitground;
+    bool spawnfx;
+
+    void Update()
+    {
+        hitground = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (hitground == true)
+        {
+            Drop();
+        }
+    }
+
+    public void Drop()
+    {
+        if (spawnfx == false)
+        {
+            spawnfx = true;
+            Instantiate(dropEffect, transform.position, Quaternion.identity);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+    }
+}
