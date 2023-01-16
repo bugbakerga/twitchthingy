eec3075a768cc45c699ff827ed406a64bb1ee2de
bugbakerga <wbrenden82@gmail.com>
2023-01-15 21:08:45 -0600

    added lava map

---
+++ TwitchProject2/Assets/Scripts/Spinning.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinning : MonoBehaviour
+{
+    public float xSpeed;
+    public float ySpeed;
+    public float zSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xSpeed * Time.deltaTime, ySpeed * Time.deltaTime, zSpeed * Time.deltaTime, Space.Self);
+    }
+}
