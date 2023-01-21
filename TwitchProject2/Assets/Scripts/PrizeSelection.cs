f0e8af6acc1ff696f277661fd7bba00741104384
bugbakerga <wbrenden82@gmail.com>
2023-01-21 12:30:26 -0600

    added ui for match settings
    started changing gfx for devil jigrjgijr

---
+++ TwitchProject2/Assets/Scripts/PrizeSelection.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrizeSelection : MonoBehaviour
+{
+    public string equipedname;
+    public Toggle visual;
+    public Text visualtext;
+
+    public bool isOn;
+    int retrievednum;
+
+    void Start()
+    {
+        retrievednum = PlayerPrefs.GetInt(equipedname, 1);
+        if(retrievednum == 1)
+        {
+            isOn = true;
+            visual.isOn = true;
+            visualtext.text = "Equipped";
+        }
+        else
+        {
+            isOn = false;
+            visual.isOn = false;
+            visualtext.text = "Equip";
+        }
+    }
+
+    public void Change()
+    {
+        if (isOn == true)
+        {
+            isOn = false;
+            PlayerPrefs.SetInt(equipedname, 0);
+            visual.isOn = false;
+            visualtext.text = "Equip";
+        }
+        else
+        {
+            isOn = true;
+            PlayerPrefs.SetInt(equipedname, 1);
+            visual.isOn = true;
+            visualtext.text = "Equipped";
+        }
+    }
+
+}
