e17df5292b47c38567dd1cb8176f55bc985d7e48
bugbakerga <wbrenden82@gmail.com>
2023-04-19 14:11:42 -0500

    added more details to winscreen

--- TwitchProject2/Assets/Scripts/AnimTrigger.cs
+++ TwitchProject2/Assets/Scripts/AnimTrigger.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class AnimTrigger : MonoBehaviour
 {
     public Animator uianimator;
+    public int maxAnims = 2;
+
+    Animator MainAnimator;
+    
+    void Start()
+    {
+        int animIndex = Random.Range(1, maxAnims);
+        MainAnimator = gameObject.GetComponent<Animator>();
+        MainAnimator.SetInteger("WinAnim", animIndex);
+    }
     
     public void TriggerUI()
     {
